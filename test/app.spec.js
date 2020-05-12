6b82e18c833565205df7e56c1c8eb93877d4c16f
bleek42 <brandonleek42@gmail.com>
2020-05-11 22:59:54 -0400

    start over deploy

---
+++ test/app.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable strict */
+const app = require('../src/app');
+
+describe('App', () => {
+
+  it('GET / responds with 200 containing a list of endpoints', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200)
+      .then(res => {
+        expect(res.body).to.have.property('endpoints');
+      });
+  });
+  
+});
